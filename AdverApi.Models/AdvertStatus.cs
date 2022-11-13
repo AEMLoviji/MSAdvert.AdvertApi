6062261e743de47887757acf622b50f3cc70b38d
aemloviji <aemloviji@gmail.com>
2022-11-13 23:56:48 +0300

    feat: implement DynamoDb based storage

---
+++ AdverApi.Models/AdvertStatus.cs
@@ -0,0 +1,7 @@
+﻿namespace AdvertApi.Models;
+
+public enum AdvertStatus
+{
+    Pending = 1,
+    Active = 2
+}
---
+++ AdvertApi/Controllers/Advert.cs
@@ -0,0 +1,56 @@
+using AdvertApi.Models;
+using AdvertApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdvertApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/adverts")]
+    public class Advert : ControllerBase
+    {
+        private readonly IAdvertStorageService _advertStorageService;
+
+        public Advert(IAdvertStorageService advertStorageService)
+        {
+            _advertStorageService = advertStorageService;
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(201, Type = typeof(CreateAdvertResponse))]
+        public async Task<IActionResult> Create(AdvertModel model)
+        {
+            string recordId;
+            try
+            {
+                recordId = await _advertStorageService.Add(model);
+            }
+            // TODO: I know exceptions should be handled in a centralized way using middleware for example
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+
+            return Accepted(new CreateAdvertResponse() { Id = recordId });
+        }
+
+        [HttpPut]
+        [Route("Confirm")]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(200)]
+        public async Task<IActionResult> Confirm(ConfirmAdvertModel model)
+        {
+            try
+            {
+                await _advertStorageService.Confirm(model);
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+
+            return Ok();
+        }
+    }
+}
